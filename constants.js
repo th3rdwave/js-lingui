7af9697231a642aeec09378c769590124c3ca01e
Janic Duplessis <janicduplessis@gmail.com>
2021-08-02 22:12:27 +0000

    gitpkg

---
+++ constants.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.VALUES = exports.EXTRACT_MARK = exports.COMMENT = exports.MESSAGE = exports.ID = void 0;
+var ID = "id";
+exports.ID = ID;
+var MESSAGE = "message";
+exports.MESSAGE = MESSAGE;
+var COMMENT = "comment";
+exports.COMMENT = COMMENT;
+var EXTRACT_MARK = "i18n";
+exports.EXTRACT_MARK = EXTRACT_MARK;
+var VALUES = "values";
+exports.VALUES = VALUES;
